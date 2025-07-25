2b38805453dd09b8b0ef5c32b82fca3caa68e030
Kriti <kritishah989@gmail.com>
2025-07-25 00:55:50 -0400

    Homepage Added

---
+++ dance-app/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the Dance App 💃🕺</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+};
+
+export default HomePage;
